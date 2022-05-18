156612bad9d96e87971d986261add006581c8081
Lucho Romero <go.romerolae@gmail.com>
2022-05-18 06:08:01 -0500

    Encapsulamos todos los calculos dentro de funciones

--- figuras.js
+++ figuras.js
@@ -1,62 +1,90 @@
 // Codigo del cuadrado 
 console.group("cuadrados")
 
-const ladoCuadrado = 5;
+// const ladoCuadrado = 5;
+// console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm` )
 
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm` )
-
-const perimetroCuadrado = ladoCuadrado* 4;
-
-console.log(`El perimetro del cuadrado  mide: ${perimetroCuadrado} cm` )
+function  perimetroCuadrado (lado){
+    return lado* 4;
+}
 
 
-const areaCuadrado = ladoCuadrado* ladoCuadrado;
+//console.log(`El perimetro del cuadrado  mide: ${perimetroCuadrado} cm` )
 
-console.log(`El area del cuadrado mide: ${areaCuadrado} cm^2`)
+
+function areaCuadrado (lado){
+   
+   return  lado*lado;
+}
+
+
+//console.log(`El area del cuadrado mide: ${areaCuadrado} cm^2`)
 
 console.groupEnd()
 // Codigo del triangulo
 
 console.group("Triangulos")
 
-const ladoTriangulo1 =6;
+// const ladoTriangulo1 =6;
+// const ladoTriangulo2 =6;
+// const baseTriangulo =4;
+// const alturaTriangulo =5.5;
 
-const ladoTriangulo2 =6;
+function areaTriangulo(base, altura){
+ return (base*altura)/2;
+}
 
-const baseTriangulo =4;
 
-const alturaTriangulo =5.5;
-
-const areaTriangulo = (baseTriangulo*alturaTriangulo)/2
-
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo
-
-console.log(`Los lados del triangulo miden: ${ladoTriangulo1} cm, ${ladoTriangulo2} cm, ${baseTriangulo} cm`);
-
-console.log(`La altura del triangulo mide: ${alturaTriangulo} cm` )
-
-console.log(`El perimetro del triangulo  mide: ${perimetroTriangulo} cm` )
-
-console.log(`El area del triangulo mide: ${areaTriangulo} cm^2` )
+function perimetroTriangulo (lado1, lado2, base){
+   return  lado1 + lado2 + base;
+}
+// console.log(`Los lados del triangulo miden: ${ladoTriangulo1} cm, ${ladoTriangulo2} cm, ${baseTriangulo} cm`);
+// console.log(`La altura del triangulo mide: ${alturaTriangulo} cm` )
+// console.log(`El perimetro del triangulo  mide: ${perimetroTriangulo} cm` )
+// console.log(`El area del triangulo mide: ${areaTriangulo} cm^2` )
 console.groupEnd
+
+
+// Codigo del circulo
+console.group("Circulo")
+
+function diametroCirculo(radio){
+    return radio*2;
+}
+
+
+const PI = Math.PI;
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+function areaCirculo(radio){
+    return (radio*radio)*PI;
+}
+console.groupEnd()
+
 
 // Codigo del Romboide
 console.group("Romboide")
-const alturaRomboide = 5;
+// const alturaRomboide = 5;
+// const baseRomboide = 7;
+// const areaRomboide = baseRomboide*alturaRomboide;
+// const perimetroRomboide = 2*baseRomboide + 2*alturaRomboide;
 
-const baseRomboide = 7;
+function areaRomboide(base, altura){
+    return base*altura;
+}
 
-const areaRomboide = baseRomboide*alturaRomboide;
+function perimetroRomboide(base, altura){
+    return 2*base + 2*altura;
+}
 
-const perimetroRomboide = 2*baseRomboide + 2*alturaRomboide;
+// console.log(`La altura del Romboide mide: ${alturaRomboide} cm` )
+// console.log(`El base del Romboide mide: ${baseRomboide} cm` )
+// console.log(`La area del Romboide mide: ${areaRomboide} cm^2` )
+// console.log(`El perimetro del Romboide  mide: ${perimetroRomboide} cm` )
 
-console.log(`La altura del Romboide mide: ${alturaRomboide} cm` )
-
-console.log(`El base del Romboide mide: ${baseRomboide} cm` )
-
-console.log(`La area del Romboide mide: ${areaRomboide} cm^2` )
-
-console.log(`El perimetro del Romboide  mide: ${perimetroRomboide} cm` )
 console.groupEnd()
 
 
